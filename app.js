64d7d165a24ba88f64b8d2177f8863b9dba403ef
hasnain393 <hasnains312@gmail.com>
2025-03-20 18:02:37 -0400

    with redis cache

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
+import { createClient } from 'redis';
 
 const app = express();
 
 app.use(express.json());
 
+const client = await createClient()
+    .on('error', (err) => console.log('Redis Client Error', err))
+    .connect();
 
 mongoose.connect('mongodb://root:root@localhost:27017/admin?authSource=admin');
 
@@ -17,12 +20,18 @@ const productSchema = new mongoose.Schema({
     specs: Object,
 });
 
-const Product = mongoose.model('Product', productSchema, 'node_cache.products');
-
+const Product = mongoose.model('Product', productSchema,'node_cache.products');
 
 app.get('/api/products', async (req, res) => {
-   
+    const key = generateCacheKey(req);
 
+    const cachedProducts = await client.get(key);
+    if (cachedProducts) {
+        console.log('Cache hit');
+        res.json(JSON.parse(cachedProducts));
+        return;
+    }
+    console.log('Cache miss');
 
     const query = {};
     if (req.query.category) {
@@ -30,13 +39,24 @@ app.get('/api/products', async (req, res) => {
     }
 
     const products = await Product.find(query);
- 
-    
+
+    if (products.length) {
+        await client.set(key, JSON.stringify(products));
+    }
 
     res.json(products);
 });
 
+function generateCacheKey(req) {
+    const baseUrl = req.path.replace(/^\/+|\/+$/g, '').replace(/\//g, ':');
+    const params = req.query;
+    const sortedParams = Object.keys(params)
+        .sort()
+        .map((key) => `${key}=${params[key]}`)
+        .join('&');
 
+    return sortedParams ? `${baseUrl}:${sortedParams}` : baseUrl;
+}
 
 app.put('/api/products/:id', async (req, res) => {
     const productId = req.params.id;
@@ -55,12 +75,16 @@ app.put('/api/products/:id', async (req, res) => {
         });
     }
 
+    const listCacheKey = 'api:products*';
+    const keys = await client.keys(listCacheKey);
+    if (keys.length > 0) {
+        await client.del(keys);
+    }
+
     res.json({
         success: true,
         message: 'Product updated successfully',
     });
 });
-
-
 
 app.listen(4000, () => console.log('Server listening on port 4000'));
